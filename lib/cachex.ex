992ff82f935569ed1f5a335182ffda17edcf3133
yang <yang@blueshift.com>
2023-10-14 16:19:46 -0700

    support params

--- lib/cachex.ex
+++ lib/cachex.ex
@@ -1,12 +1,12 @@
 defmodule Cachex do
-  @callback fetch(key :: term(), opts :: keyword()) :: {:ok, term()} | {:error, term()}
+  @callback fetch(key :: atom(), params :: map(), opts :: keyword()) ::
+              {:ok, term()} | {:error, term()}
 
   def compile_config(opts) do
-    {cache, opts} = Keyword.pop!(opts, :cache)
     {timeout, opts} = Keyword.pop(opts, :timeout, 5_000)
-    {expiry, opts} = Keyword.pop(opts, :expiry, 60_000)
+    {expiry, _opts} = Keyword.pop(opts, :expiry, 60_000)
 
-    {cache, timeout, expiry, opts}
+    {timeout, expiry}
   end
 
   defmacro __using__(opts) do
@@ -15,44 +15,39 @@ defmodule Cachex do
 
       @behaviour Cachex
 
-      {cache, timeout, expiry, opts} = Cachex.compile_config(opts)
+      {timeout, expiry} = Cachex.compile_config(opts)
 
-      @cache cache
       @timeout timeout
       @expiry expiry
 
-      def start_link(opts) do
-        GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+      def start_link([]) do
+        GenServer.start_link(__MODULE__, [], name: __MODULE__)
       end
 
-      def get(key, opts) do
+      def get(key, params, opts) do
         {timeout, opts} = Keyword.pop(opts, :timeout, @timeout)
         {expiry, opts} = Keyword.pop(opts, :expiry, @expiry)
 
         now = System.monotonic_time(:millisecond)
 
-        case :ets.lookup(cache_table(), key) do
-          [{^key, {value, ts}}] when now <= ts + expiry ->
+        case :ets.lookup(__MODULE__, {key, params}) do
+          [{_, {value, ts}}] when now <= ts + expiry ->
             {:ok, value}
 
-          [{^key, {value, _ts}}] ->
-            GenServer.cast(__MODULE__, {:fetch, key, opts})
+          [{_, {value, _ts}}] ->
+            GenServer.cast(__MODULE__, {:fetch, key, params, opts})
             {:ok, value}
 
           [] ->
-            GenServer.call(__MODULE__, {:fetch, key, opts, expiry}, timeout)
+            GenServer.call(__MODULE__, {:fetch, key, params, opts, expiry}, timeout)
         end
       end
 
-      defp cache_table() do
-        String.to_atom("cachex_#{@cache}")
-      end
-
       @impl true
-      def init(opts) do
+      def init([]) do
         Process.flag(:trap_exit, true)
 
-        :ets.new(cache_table(), [
+        :ets.new(__MODULE__, [
           :named_table,
           :set,
           :public,
@@ -62,18 +57,16 @@ defmodule Cachex do
 
         {:ok, sup} = Task.Supervisor.start_link()
 
-        {:ok, %{sup: sup, opts: opts, refs: %{}}}
+        {:ok, %{sup: sup, refs: %{}}}
       end
 
       @impl true
-      def handle_cast({:fetch, key, opts}, %{sup: sup, opts: options, refs: refs} = state) do
-        case Map.get(refs, key) do
+      def handle_cast({:fetch, key, params, opts}, %{sup: sup, refs: refs} = state) do
+        case Map.get(refs, {key, params}) do
           nil ->
-            opts = Keyword.merge(options, opts)
+            task = Task.Supervisor.async_nolink(sup, fn -> fetch(key, params, opts) end)
 
-            task = Task.Supervisor.async_nolink(sup, fn -> fetch(key, opts) end)
-
-            refs = Map.put(refs, key, {task.ref, [], System.monotonic_time()})
+            refs = Map.put(refs, {key, params}, {task.ref, [], System.monotonic_time()})
             {:noreply, %{state | refs: refs}}
 
           {ref, _froms, _ts} ->
@@ -83,37 +76,33 @@ defmodule Cachex do
 
       @impl true
       def handle_call(
-            {:fetch, key, opts, expiry},
+            {:fetch, key, params, opts, expiry},
             from,
-            %{sup: sup, opts: options, refs: refs} = state
+            %{sup: sup, refs: refs} = state
           ) do
         now = System.monotonic_time(:millisecond)
 
-        case {:ets.lookup(cache_table(), key), Map.get(refs, key)} do
-          {[{^key, {value, ts}}], _ref} when now <= ts + expiry ->
+        case {:ets.lookup(__MODULE__, {key, params}), Map.get(refs, {key, params})} do
+          {[{_, {value, ts}}], _ref} when now <= ts + expiry ->
             {:reply, {:ok, value}, state}
 
-          {[{^key, {value, _ts}}], nil} ->
-            opts = Keyword.merge(options, opts)
+          {[{_, {value, _ts}}], nil} ->
+            task = Task.Supervisor.async_nolink(sup, fn -> fetch(key, params, opts) end)
 
-            task = Task.Supervisor.async_nolink(sup, fn -> fetch(key, opts) end)
-
-            refs = Map.put(refs, key, {task.ref, [], System.monotonic_time()})
+            refs = Map.put(refs, {key, params}, {task.ref, [], System.monotonic_time()})
             {:reply, {:ok, value}, %{state | refs: refs}}
 
-          {[{^key, {value, _ts}}], _ref} ->
+          {[{_, {value, _ts}}], _ref} ->
             {:reply, {:ok, value}, state}
 
           {[], nil} ->
-            opts = Keyword.merge(options, opts)
+            task = Task.Supervisor.async_nolink(sup, fn -> fetch(key, params, opts) end)
 
-            task = Task.Supervisor.async_nolink(sup, fn -> fetch(key, opts) end)
-
-            refs = Map.put(refs, key, {task.ref, [from], System.monotonic_time()})
+            refs = Map.put(refs, {key, params}, {task.ref, [from], System.monotonic_time()})
             {:noreply, %{state | refs: refs}}
 
           {[], {ref, froms, ts}} ->
-            refs = Map.put(refs, key, {ref, [from | froms], ts})
+            refs = Map.put(refs, {key, params}, {ref, [from | froms], ts})
             {:noreply, %{state | refs: refs}}
         end
       end
@@ -122,16 +111,16 @@ defmodule Cachex do
       def handle_info({ref, {:ok, value}}, %{refs: refs} = state) do
         Process.demonitor(ref, [:flush])
 
-        {[{key, {_ref, froms, ts}}], refs} =
-          Enum.split_with(refs, &match?({_key, {^ref, _froms, _ts}}, &1))
+        {[{{key, params}, {_ref, froms, ts}}], refs} =
+          Enum.split_with(refs, &match?({{_key, _params}, {^ref, _froms, _ts}}, &1))
 
         :telemetry.execute(
           [:cachex, :fetch, :success],
           %{count: 1, duration: System.monotonic_time() - ts},
-          %{cache: @cache, key: key}
+          %{cache: __MODULE__, key: key, params: params}
         )
 
-        :ets.insert(cache_table(), {key, {value, System.monotonic_time(:millisecond)}})
+        :ets.insert(__MODULE__, {{key, params}, {value, System.monotonic_time(:millisecond)}})
 
         Enum.each(froms, &GenServer.reply(&1, {:ok, value}))
 
@@ -141,13 +130,13 @@ defmodule Cachex do
       def handle_info({ref, {:error, err}}, %{refs: refs} = state) do
         Process.demonitor(ref, [:flush])
 
-        {[{key, {_ref, froms, ts}}], refs} =
-          Enum.split_with(refs, &match?({_key, {^ref, _froms, _ts}}, &1))
+        {[{{key, params}, {_ref, froms, ts}}], refs} =
+          Enum.split_with(refs, &match?({{_key, _params}, {^ref, _froms, _ts}}, &1))
 
         :telemetry.execute(
           [:cachex, :fetch, :error],
           %{count: 1, duration: System.monotonic_time() - ts},
-          %{cache: @cache, key: key}
+          %{cache: __MODULE__, key: key, params: params}
         )
 
         Enum.each(froms, &GenServer.reply(&1, {:error, err}))
@@ -156,13 +145,13 @@ defmodule Cachex do
       end
 
       def handle_info({:DOWN, ref, :process, _pid, reason}, %{refs: refs} = state) do
-        {[{key, {_ref, froms, ts}}], refs} =
-          Enum.split_with(refs, &match?({_key, {^ref, _froms, _ts}}, &1))
+        {[{{key, params}, {_ref, froms, ts}}], refs} =
+          Enum.split_with(refs, &match?({{_key, _params}, {^ref, _froms, _ts}}, &1))
 
         :telemetry.execute(
           [:cachex, :fetch, :error],
           %{count: 1, duration: System.monotonic_time() - ts},
-          %{cache: @cache, key: key}
+          %{cache: __MODULE__, key: key, params: params}
         )
 
         Enum.each(froms, &GenServer.reply(&1, {:error, reason}))
@@ -172,17 +161,17 @@ defmodule Cachex do
 
       @impl true
       def terminate(_reason, state) do
-        :ets.delete(cache_table())
+        :ets.delete(__MODULE__)
 
         state
       end
 
       @impl Cachex
-      def fetch(_key, _opts) do
+      def fetch(_key, _params, _opts) do
         {:error, :not_implemented}
       end
 
-      defoverridable fetch: 2
+      defoverridable fetch: 3
     end
   end
 end
